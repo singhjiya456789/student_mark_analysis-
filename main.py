556babc2741506c39341d6a4641a822f285f3fde
singhjiya123 <jiyarajput2082@gmail.com>
2026-05-15 21:42:26 +0545

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import numpy as np
+Marks=input("Enter the marks:")
+Marks= np.array(Marks.split(), dtype= int)
+
+Mean= np.mean(Marks)
+print("Mean of marks :",Mean)
+
+Maximum= np.max(Marks)
+print("Maximum of marks :",Maximum)
+
+Minimum = np.min(Marks)
+print("Minimum of marks :",Minimum)
+
+print("Topper:",Maximum)
+
+Above_avg = Marks[Marks > Mean]
+print("Above average student are:",Above_avg)
